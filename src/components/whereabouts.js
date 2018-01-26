dc8dced641c2ae4047bf9e46db5aca92bc5350fa
L Barbee <bluelb84@gmail.com>
2018-01-26 12:09:53 -0500

    initial commit

---
+++ src/components/whereabouts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './whereabouts.css';
+
+export default function Whereabouts() {
+    return (
+        <div className="whereabouts">
+            <h1>Whereabouts</h1>
+        </div>
+    );
+}
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+ import Board from './components/whereabouts';
+ import './index.css';
+
+ ReactDOM.render(
+   <Whereabouts />,
+   document.getElementById('root')
+ );
