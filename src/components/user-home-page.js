d57e7e82153bb7f2567bbaf3b2ebadb1fdbd7560
L Barbee <bluelb84@gmail.com>
2018-01-31 06:28:53 -0500

    user status component initial

--- src/components/user-home-page.js
+++ src/components/user-home-page.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import TitleCard from './title-card';
 import TeamMotto from './team-motto';
+import UserStatus from './user-status';
 import './user-home-page.css';
 
-export default function UserHome() {
+export default function UserHome(props) {
     return (
         <div className="user-home-page">
         	<header role="banner">
             	<TitleCard title='Your Team Name' />
             	<TeamMotto motto='This is the bestest team!' />
             </header>
-            <p>Welcome to the user home page!</p>
+            <main>
+            	<h3>Team {props.teamName} Whereabouts:</h3>
+            	<UserStatus usrname="Yoothee Yuser" location="desk" activity="HTML wireframes" imgSrc="https://placeimg.com/75/75/any"/>
+            </main>
             <Link to='/'>Whereabouts Home</Link>
         </div>
     );
---
+++ src/components/user-status.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './user-status.css';
+
+export default function UserStatus(props) {
+    return (
+        <div className="user-status">
+        	<div className="usr-pic">
+	        	<img src={props.imgSrc}></img>
+	        	<p>{props.usrname}</p>
+        	</div>
+        	<ul>
+        		<li>Where: {props.location}</li>
+        		<li>What: {props.activity}</li>
+        	</ul>
+        </div>
+    );
+};
+
+UserStatus.defaultProps = {
+
+};
