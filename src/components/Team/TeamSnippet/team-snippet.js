bb50c87848c3486563ab11b8d5562fa8fe539976
L Barbee <bluelb84@gmail.com>
2018-02-11 15:57:33 -0500

    styling to make it more apparent to click on team

--- src/components/Team/TeamSnippet/team-snippet.js
+++ src/components/Team/TeamSnippet/team-snippet.js
@@ -5,8 +5,10 @@ import './team-snippet.css';
 export default function TeamSnippet(props) {
 	return (
 	    <div className="team-snippet" index={props.teamId} onClick={props.onClick}>
-	    	<img src={props.imgSrc} alt={props.imgAlt} />
-	    	<h4>{props.name}</h4>
+	    	<div className="team-snippet--hover">
+		    	<img className="team-img" src={props.imgSrc} alt={props.imgAlt} />
+		    	<h4>{props.name}</h4>
+	    	</div>
 	    	<p>{props.motto}</p>
 	    </div>
 	);
