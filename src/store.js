2f089666bb103596628a72c82d5d3975f7abd11c
L Barbee <bluelb84@gmail.com>
2018-02-19 15:00:24 -0500

    removed firebase reducer

--- src/store.js
+++ src/store.js
@@ -1,23 +1,5 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore } from 'redux';
 
-import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { whereaboutsReducer } from './reducers';
 
-import firebase from './firebase';
-
-const config = {
-  userProfile: 'users', // firebase root where user profiles are stored
-  enableLogging: false, // enable/disable Firebase's database logging
-}
-
-const rootReducer = combineReducers({
-	firebase: firebaseReducer,
-	whereabouts: whereaboutsReducer
-});
-
-// Add redux Firebase to compose
-const createStoreWithFirebase = compose(
-  reactReduxFirebase(firebase, config)
-)(createStore)
-
-export default createStoreWithFirebase(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore(whereaboutsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
