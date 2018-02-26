b7d214ab5e27f5cbf650446c57ae016e3704f297
L Barbee <bluelb84@gmail.com>
2018-02-25 22:00:28 -0500

    successful AJAX request, pre-redux thunk, stateful component

--- src/components/Pages/UserLandingPage/user-landing-page.js
+++ src/components/Pages/UserLandingPage/user-landing-page.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {API_BASE_URL} from '../../config';
+// import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { API_BASE_URL } from '../../../config';
 
 import TitleCard from '../../Headers/TitleCard/title-card';
 import TeamSnippet from '../../Team/TeamSnippet/team-snippet';
 
 import './user-landing-page.css';
 
-export class UserLandingPage extends React.Component {
+export default class UserLandingPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            users,
+            users: [],
+            teams: [],
             error: null,
             loading: false
         };
     }
 
-    componentDidMount() {
-        this.loadUserTeams();
+    componentWillMount() {
+        this.loadUserData();
+        this.loadTeamData();
     }
 
-    loadUserTeams() {
+    loadUserData() {
         this.setState({
             error: null,
             loading: true
@@ -34,15 +36,41 @@ export class UserLandingPage extends React.Component {
                 }
                 return res.json();
             })
-            then(users => 
+            .then(data => {
                 this.setState({
-                    users,
+                    users: data.users,
                     loading: false
-                })
-            )
+                });
+            })
             .catch(err => 
                 this.setState({
                     error: 'Could not load user information',
+                    loading: false
+                })
+            );
+    }
+
+    loadTeamData() {
+        this.setState({
+            error: null,
+            loading: true
+        });
+        return fetch(`${API_BASE_URL}/teams`)
+            .then(res => {
+                if (!res.ok) {
+                    return Promise.reject(res.statusText);
+                }
+                return res.json();
+            })
+            .then(data => {
+                this.setState({
+                    teams: data.teams,
+                    loading: false
+                });
+            })
+            .catch(err => 
+                this.setState({
+                    error: 'Could not load team information',
                     loading: false
                 })
             );
@@ -53,54 +81,68 @@ export class UserLandingPage extends React.Component {
     }
 
     render() {
-        const userIdParam = parseInt(this.props.match.params.userId, 10);
-
-        const currentUser = this.props.users.filter(user => 
-            user.userId === userIdParam
-        )[0];
-
-        const currentUserTeams = [];
         
-        this.props.teams.map((team, index) => {
-            if (team.users.includes(userIdParam)) {
-                currentUserTeams.push(team);
-            }
-            return false;
-        });
+        let body;
+        if (this.state.error) {
+            body = (<div className="message message-error">{this.state.error}</div>);
+        } else if (this.state.loading) {
+            body = (<div className="message message-default">Loading your information...</div>);
+        } else {
+            const userIdParam = this.props.match.params.userId;
 
-        const teams = currentUserTeams.map((team, index) => (
-            <li className="user-team" key={index}>
-                <TeamSnippet {...team} index={index} teamId={team.teamId} onClick={this.handleTeamClick.bind(this, team.teamId)} />
-            </li>
-        ));
+            const currentUser = this.state.users.filter(user => 
+                user.userId === userIdParam
+            )[0];
 
-        const userWelcome = `Welcome ${currentUser.usrname}`;
-    
+            const currentUserTeams = [];
+            
+            this.state.teams.map((team, index) => {
+                if (team.users.includes(userIdParam)) {
+                    currentUserTeams.push(team);
+                }
+                return false;
+            });
+
+            const teams = currentUserTeams.map((team, index) => (
+                <li className="user-team" key={index}>
+                    <TeamSnippet {...team} index={index} teamId={team.teamId} onClick={this.handleTeamClick.bind(this, team.teamId)} />
+                </li>
+            ));
+
+            const userWelcome = `Welcome ${currentUser.usrname}`;
+            
+            body = (
+                <div>
+                    <header role="banner">
+                        <TitleCard title={userWelcome} />
+                    </header>
+                    <main>
+                        <h3>Your Whereabouts Teams:</h3>
+                        <div className="user-whereabouts-container">
+                            <ul className="user-teams-list">
+                                {teams}
+                            </ul>
+                        </div>
+                    </main>
+                    <Link to='/'>Whereabouts Home</Link>
+                </div>
+            );
+        }
+
         return (
             <div className="user-landing-page">
-                <header role="banner">
-                    <TitleCard title={userWelcome} />
-                </header>
-                <main>
-                    <h3>Your Whereabouts Teams:</h3>
-                    <div className="user-whereabouts-container">
-                        <ul className="user-teams-list">
-                            {teams}
-                        </ul>
-                    </div>
-                </main>
-                <Link to='/'>Whereabouts Home</Link>
+                {body}
             </div>
         );
     }
 };
 
-const mapStateToProps = (state, props) => ({
-    teams: state.teams,
-    users: state.users
-});
+// const mapStateToProps = (state, props) => ({
+//     teams: state.teams,
+//     users: state.users
+// });
 
-export default connect(mapStateToProps)(UserLandingPage);
+// export default connect(mapStateToProps)(UserLandingPage);
 
 
 
