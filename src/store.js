8a90c6baed339746efc8a5331c6b7c0d22fd3b68
L Barbee <bluelb84@gmail.com>
2018-02-26 18:55:54 -0500

    added Redux Thunk

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import { whereaboutsReducer } from './reducers';
 
-export default createStore(whereaboutsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore(whereaboutsReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
