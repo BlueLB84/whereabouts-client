fe4455fec8eecbf554ab923559d5ed9b2282b64c
L Barbee <bluelb84@gmail.com>
2018-02-26 19:38:06 -0500

    reconnected to Redux props with thunk

--- src/components/Pages/UserLandingPage/user-landing-page.js
+++ src/components/Pages/UserLandingPage/user-landing-page.js
@@ -1,79 +1,18 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { API_BASE_URL } from '../../../config';
+
+import { fetchUsers, fetchTeams } from '../../../actions';
 
 import TitleCard from '../../Headers/TitleCard/title-card';
 import TeamSnippet from '../../Team/TeamSnippet/team-snippet';
 
 import './user-landing-page.css';
 
-export default class UserLandingPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-            teams: [],
-            error: null,
-            loading: false
-        };
-    }
-
-    componentWillMount() {
-        this.loadUserData();
-        this.loadTeamData();
-    }
-
-    loadUserData() {
-        this.setState({
-            error: null,
-            loading: true
-        });
-        return fetch(`${API_BASE_URL}/users`)
-            .then(res => {
-                if (!res.ok) {
-                    return Promise.reject(res.statusText);
-                }
-                return res.json();
-            })
-            .then(data => {
-                this.setState({
-                    users: data.users,
-                    loading: false
-                });
-            })
-            .catch(err => 
-                this.setState({
-                    error: 'Could not load user information',
-                    loading: false
-                })
-            );
-    }
-
-    loadTeamData() {
-        this.setState({
-            error: null,
-            loading: true
-        });
-        return fetch(`${API_BASE_URL}/teams`)
-            .then(res => {
-                if (!res.ok) {
-                    return Promise.reject(res.statusText);
-                }
-                return res.json();
-            })
-            .then(data => {
-                this.setState({
-                    teams: data.teams,
-                    loading: false
-                });
-            })
-            .catch(err => 
-                this.setState({
-                    error: 'Could not load team information',
-                    loading: false
-                })
-            );
+export class UserLandingPage extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(fetchUsers);
+        this.props.dispatch(fetchTeams);
     }
 
     handleTeamClick(index) {
@@ -90,13 +29,13 @@ export default class UserLandingPage extends React.Component {
         } else {
             const userIdParam = this.props.match.params.userId;
 
-            const currentUser = this.state.users.filter(user => 
+            const currentUser = this.props.users.filter(user => 
                 user.userId === userIdParam
             )[0];
 
             const currentUserTeams = [];
             
-            this.state.teams.map((team, index) => {
+            this.props.teams.map((team, index) => {
                 if (team.users.includes(userIdParam)) {
                     currentUserTeams.push(team);
                 }
@@ -137,12 +76,12 @@ export default class UserLandingPage extends React.Component {
     }
 };
 
-// const mapStateToProps = (state, props) => ({
-//     teams: state.teams,
-//     users: state.users
-// });
+const mapStateToProps = state => ({
+    teams: state.teams,
+    users: state.users
+});
 
-// export default connect(mapStateToProps)(UserLandingPage);
+export default connect(mapStateToProps)(UserLandingPage);
 
 
 
