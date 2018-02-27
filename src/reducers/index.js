09c62bca7efcd52ac090211fbb591b088dc05cd8
L Barbee <bluelb84@gmail.com>
2018-02-26 19:37:27 -0500

    changed reducer to a switch function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,54 +1,115 @@
 import * as actions from '../actions';
 
-const initialState = {};
+const initialState = {
+    users: [],
+    teams: [],
+    loading: null,
+    err: null
+};
 
 export const whereaboutsReducer = (state=initialState, action) => {
-    if (action.type === actions.ADD_TEAM) {
-        return {
-            ...state,
-            teams: action.teamId
-        }
-    } else if (action.type === actions.ADD_BULLETIN) {
-        let teamIndex;
-
-        state.teams.map((team, index) => {
-            if (team.teamId === action.teamId) {
-                teamIndex = index;
+    switch(action.type) {
+        case `${actions.ADD_TEAM}` : {
+            return {
+                ...state,
+                teams: action.teamId
             }
-            return false;
-        });
+        }
+        case `${actions.ADD_BULLETIN}` : {
+            let teamIndex;
 
-        const newBulletinsArr = [...state.teams[teamIndex].bulletins, action.bulletins];
+            state.teams.map((team, index) => {
+                if (team.teamId === action.teamId) {
+                    teamIndex = index;
+                }
+                return false;
+            });
 
-        return {
-            ...state,
-            teams: state.teams.map((team, index) => index === teamIndex ? {...team, bulletins: newBulletinsArr} : team)
-        }
-    } else if (action.type === actions.ADD_USER) {
-        return {
-            ...state,
-            users: action.userId
-        }
-    } else if (action.type === actions.UPDATE_USER) {
-        return {
-            ...state,
-            users: {...state.users, ...action.userId}
-        }
-    } else if (action.type === actions.ADD_WHEREABOUTS) {
-        let userIndex;
-        state.users.map((user, index) => {
-            if (user.userId === action.userId) {
-                userIndex = index;
+            const newBulletinsArr = [...state.teams[teamIndex].bulletins, action.bulletins];
+
+            return {
+                ...state,
+                teams: state.teams.map((team, index) => index === teamIndex ? {...team, bulletins: newBulletinsArr} : team)
             }
-            return false;
-        });
-
-        return {
-            ...state,
-            users: state.users.map((user, index) => index === userIndex ? {...user, whereabouts: {...action.whereabouts}} : user)
         }
-    } 
-    return state;
-};
+        case `${actions.ADD_USER}` : {
+            return {
+                ...state,
+                users: action.userId
+            }
+        }
+        case `${actions.UPDATE_USER}` : {
+            return {
+                ...state,
+                users: {...state.users, ...action.userId}
+            }
+        }
+        case `${actions.ADD_WHEREABOUTS}` : {
+            let userIndex;
+            state.users.map((user, index) => {
+                if (user.userId === action.userId) {
+                    userIndex = index;
+                }
+                return false;
+            });
+
+            return {
+                ...state,
+                users: state.users.map((user, index) => index === userIndex ? {...user, whereabouts: {...action.whereabouts}} : user)
+            }
+        }
+        default : return state;
+    }
+}
+
+
+// export const whereaboutsReducer = (state=initialState, action) => {
+//     if (action.type === actions.ADD_TEAM) {
+//         return {
+//             ...state,
+//             teams: action.teamId
+//         }
+//     } else if (action.type === actions.ADD_BULLETIN) {
+//         let teamIndex;
+
+//         state.teams.map((team, index) => {
+//             if (team.teamId === action.teamId) {
+//                 teamIndex = index;
+//             }
+//             return false;
+//         });
+
+//         const newBulletinsArr = [...state.teams[teamIndex].bulletins, action.bulletins];
+
+//         return {
+//             ...state,
+//             teams: state.teams.map((team, index) => index === teamIndex ? {...team, bulletins: newBulletinsArr} : team)
+//         }
+//     } else if (action.type === actions.ADD_USER) {
+//         return {
+//             ...state,
+//             users: action.userId
+//         }
+//     } else if (action.type === actions.UPDATE_USER) {
+//         return {
+//             ...state,
+//             users: {...state.users, ...action.userId}
+//         }
+//     } else if (action.type === actions.ADD_WHEREABOUTS) {
+//         let userIndex;
+//         state.users.map((user, index) => {
+//             if (user.userId === action.userId) {
+//                 userIndex = index;
+//             }
+//             return false;
+//         });
+
+//         return {
+//             ...state,
+//             users: state.users.map((user, index) => index === userIndex ? {...user, whereabouts: {...action.whereabouts}} : user)
+//         }
+//     } 
+//     return state;
+// };
 
 
