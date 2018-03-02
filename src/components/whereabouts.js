039b91dfe3b319f44eaa6cc4006a850992744f9e
L Barbee <bluelb84@gmail.com>
2018-03-01 20:58:34 -0500

    removed extra container div

--- src/components/whereabouts.js
+++ src/components/whereabouts.js
@@ -5,34 +5,33 @@ import {
     Switch,
     Link
 } from 'react-router-dom';
+
 import Nav from './Nav/nav';
 import TitleCard from './Headers/TitleCard/title-card';
 import LandingPage from './Pages/LandingPage/landing-page';
-import UserLandingPageContainer from './Containers/user-landing-page-container';
-import TeamHome from './Pages/TeamHome/team-home-page';
+import UserTeamLandingPageContainer from './Containers/user-team-landing-page-container';
 import SignupForm from './Forms/SignupForm/signup-form';
 import Footer from './Footer/footer';
+
 import './whereabouts.css';
 
 export default function Whereabouts() {
     return (
         <Router>
             <div className='whereabouts'>
-                
-                    <Nav />
-                    <header>
-                        <Link to='/'><TitleCard title='Whereabouts' /></Link>
-                    </header>
-                    <main>
-                        <Switch>
-                            <Route exact path='/' component={LandingPage} />
-                            <Route exact path='/user/:userId' component={UserLandingPageContainer} />
-                            <Route exact path='/team/:teamId' component={TeamHome} />
-                            <Route exact path='/signin-signup' component={SignupForm} />
-                        </Switch>
-                    </main>
-                    <Footer />
-                
+                <Nav />
+                <header>
+                    <Link to='/'><TitleCard title='Whereabouts' /></Link>
+                </header>
+                <main>
+                    <Switch>
+                        <Route exact path='/' component={LandingPage} />
+                        <Route exact path='/user/:userId' component={UserTeamLandingPageContainer} />
+                        <Route exact path='/team/:teamId' component={UserTeamLandingPageContainer} />
+                        <Route exact path='/signin-signup' component={SignupForm} />
+                    </Switch>
+                </main>
+                <Footer />
             </div>
         </Router>
     );
